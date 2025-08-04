912b696b1c8c14b597afa8cc2f45f0766ad8b729
Bartłomiej Rudecki <przekichane@icloud.com>
2025-08-04 22:45:53 +0200

    feat(homepage): add social links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,30 @@
+<script lang="ts">
+  import LineMdGithubLoop from '~icons/line-md/github-loop';
+  import LineMdBuyMeACoffeeFilled from '~icons/line-md/buy-me-a-coffee-filled';
+  import LineMdTelegram from '~icons/line-md/telegram';
+  import LineMdFacebook from '~icons/line-md/facebook';
+</script>
+
 <div class="flex size-full items-center justify-center">
   <div class="flex flex-col items-center gap-4 text-center">
-    <img src="https://github.com/windeqk.png" alt="windeqk" class="size-48 rounded-full" />
-    <h2 class="text-xl font-bold">Windeqk</h2>
+    <a href="https://github.com/windeqk">
+      <img src="https://github.com/windeqk.png" alt="windeqk" class="size-48 rounded-full" />
+    </a>
+    <h2 class="text-3xl font-bold">Windeqk</h2>
     <p>aka Jacek Frytkownica</p>
+    <ul class="flex flex-row gap-2 text-2xl">
+      <a href="https://github.com/windeqk">
+        <LineMdGithubLoop />
+      </a>
+      <a href="https://buymeacoffee.com/windeqk">
+        <LineMdBuyMeACoffeeFilled />
+      </a>
+      <a href="https://t.me/windeqk">
+        <LineMdTelegram />
+      </a>
+      <a href="https://facebook.com/jacek.fryta.98">
+        <LineMdFacebook />
+      </a>
+    </ul>
   </div>
 </div>
